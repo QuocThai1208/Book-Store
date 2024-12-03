31032280be6a661b47e2f7dd1d98b38d2777fbc0
QuocThai1208 <thaiqp124@gmail.com>
2024-12-03 13:30:34 +0700

    update csdl

--- app/utils.py
+++ app/utils.py
@@ -5,31 +5,31 @@ from app.models import Category, Book, Author, OrderDetail, Order
 
 # thống kê số lượng của từng loại sách
 def category_stats():
-    return db.session.query(Category.id, Category.Name, func.count(Book.id)).join(Book, Category.id.__eq__(
-        Book.Category_id), isouter=True).group_by(Category.id, Category.Name).all()
+    return db.session.query(Category.id, Category.name, func.count(Book.id)).join(Book, Category.id.__eq__(
+        Book.category_id), isouter=True).group_by(Category.id, Category.name).all()
 
 
 # Thống kê theo danh thu của sách
 def book_stats(kw=None, from_date=None, to_date=None, author=None, category=None):
-    b = db.session.query(Book.id, Book.Name, func.sum(OrderDetail.Quantity * OrderDetail.UnitPrice)).join(
-        OrderDetail, OrderDetail.Book_id.__eq__(Book.id), isouter=True).join(Order, Order.id.__eq__(
-        OrderDetail.Order_id), isouter=True).join(Author, Author.id.__eq__(Book.Author_id)).join(Category,Category.id.__eq__(  Book.Category_id)).group_by( Book.id, Book.Name)
+    b = db.session.query(Book.id, Book.name, func.sum(OrderDetail.quantity * OrderDetail.unit_price)).join(
+        OrderDetail, OrderDetail.book_id.__eq__(Book.id), isouter=True).join(Order, Order.id.__eq__(
+        OrderDetail.order_id), isouter=True).join(Author, Author.id.__eq__(Book.author_id)).join(Category,Category.id.__eq__(  Book.category_id)).group_by( Book.id, Book.name)
 
     if kw:
-        b = b.filter(Book.Name.contains(kw))
+        b = b.filter(Book.name.contains(kw))
     if from_date:
-        b = b.filter(Order.OrderDate.__ge__(from_date))  # so sánh lớn hơn
+        b = b.filter(Order.order_date.__ge__(from_date))  # so sánh lớn hơn
     if to_date:
-        b = b.filter(Order.OrderDate.__le__(to_date))  # so sánh nhỏ hơn
+        b = b.filter(Order.order_date.__le__(to_date))  # so sánh nhỏ hơn
     if author:
-        b = b.filter(Author.Name.contains(author))
+        b = b.filter(Author.name.contains(author))
     if category:
-        b = b.filter(Category.Name.contains(category))
+        b = b.filter(Category.name.contains(category))
     return b.all()
 
 #Thống kê số lượng tồn kho
 def inventory_stats(name=None):
-        b = db.session.query(Book.id, Book.Name, Book.UnitsInStock)
+        b = db.session.query(Book.id, Book.name, Book.units_in_stock)
         if name:
-            b = b.filter(Book.Name.contains(name))
+            b = b.filter(Book.name.contains(name))
         return b.all()
